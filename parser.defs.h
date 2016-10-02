6c59f52e8b78a783b99a3ead7b548f43f77c6d45
jefersonla <oi_je@hotmail.com>
2016-10-02 08:19:31 -0300

    Added some token recognization

--- parser.defs.h
+++ parser.defs.h
@@ -1,4 +1,13 @@
 /* Global defines */
 #define MAX_OUTPUT_FILENAME		100
 
-/* Pointer struct defines */
+/**
+ * Allocate and store a string of a desired size.
+ *
+ * @param str String that will be stored.
+ */
+#define allocateToken(token_str)    do{ \
+                                        $$ = malloc(sizeof(char) * (strlen(token_str) + 2)); \
+                                        strcpy($$, token_str); \
+                                    }while(0)
+
