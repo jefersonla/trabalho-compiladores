97affc20c00072441b39b2aa43545a6ca1e7b58a
jefersonla <oi_je@hotmail.com>
2016-11-10 18:27:21 +0000

    Change in the test for a interesting structure

--- test4.lua
+++ test4.lua
@@ -2,7 +2,7 @@
 --print(x)
 --print (x + 1)
 
--- For seems to be OK
+-- 'For' seems to be OK
 for i = 0, 10 do
     print((i + 2) * 2)
     if i == 0 then
@@ -12,6 +12,27 @@ for i = 0, 10 do
     print(1222333444)
 end
 
+-- For with increment
+for i = 0, 10, 3 do
+    print(i)
+end
+
+print(13334442)
+
+x = 2
+
+-- for with variable increment
+-- this should print 0 4 and 10
+for i = 0, 10, x do
+    print(i)
+    if x < 10 then
+        x = x + 2
+    end
+end
+
+print(151553444)
+
+-- This should print only from 0 to 5
 function z()
     for i = 0, 10 do
         print(i)
